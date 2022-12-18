6e98cbfe69d4037b527ee22d2394dd3b38ebd48c
enigmatik100 <m.p.gangbadja@gmail.com>
2022-12-19 00:05:59 +0100

    retrieve user's post error fixed

--- api/user/views.py
+++ api/user/views.py
@@ -4,6 +4,7 @@ from flask_restx import Namespace, Resource, fields
 
 from .. import post_namespace
 from ..models.models import Post, User
+from ..post.views import post_model
 
 user_namespace = Namespace('users', description="a namespace for the user")
 
@@ -11,6 +12,7 @@ user_namespace = Namespace('users', description="a namespace for the user")
 @user_namespace.route('/<int:user_id>/posts/')
 class PostGetCreate(Resource):
 
+    @post_namespace.marshal_with(post_model)
     def get(self, user_id):
         """
         Get all relative to specific user
